0c3b64d7f1f79e3c47b8d33569f8bdd476c2f3b7
Patrick A. McConnell <mcconnep@musc.edu>
2023-06-23 15:53:38 -0400

    Update loadECGfromACQ.m
    adding PSD plots and zero-phase butterworth filter

--- loadECGfromACQ.m
+++ loadECGfromACQ.m
@@ -26,8 +26,72 @@ disp(size(data))
 plot(data(:, 1)) 
 ecg = data(:, 1);
 
+% visualize raw input signal
+figure; 
+% ax1 = subplot(1,4,1);
+plot(ecg, 'LineWidth', 2); 
+title('Unfiltered ECG Signal Lead I');
+xlabel('time in ms'); ylabel('voltage in mV');
+hold all; 
+
 % Sampling rate
 fs = 5000;
+
+% Compute the power spectral density of the ECG signal
+[pxx, f] = pwelch(ecg, [], [], [], fs, 'onesided');
+
+% Plot the power spectral density
+figure;
+plot(f, 10*log10(pxx));
+grid on;
+title('Power Spectral Density of ECG Signal');
+xlabel('Frequency (Hz)');
+ylabel('PSD (dB/Hz)');
+
+% High-pass filter cutoff frequency (Hz)
+high_cutoff = 0.5;
+
+% Low-pass filter cutoff frequency (Hz)
+low_cutoff = 30;
+
+% Powerline frequency (Hz)
+powerline_freq = 60;
+
+% Design the Butterworth filters
+[b_high, a_high] = butter(2, high_cutoff/(fs/2), 'high');
+[b_low, a_low] = butter(2, low_cutoff/(fs/2), 'low');
+
+% Design the notch filter
+% Note that 'notch' requires Signal Processing Toolbox R2016b or later
+wo = powerline_freq/(fs/2);  
+bw = wo/35;
+[b_notch, a_notch] = iirnotch(wo, bw);
+
+% Apply the high-pass filter
+ecg_high = filtfilt(b_high, a_high, ecg);
+
+% Apply the notch filter
+ecg_notch = filtfilt(b_notch, a_notch, ecg_high);
+
+% Apply the low-pass filter
+ecg_filtered = filtfilt(b_low, a_low, ecg_notch);
+
+% Plot filtered signal
+figure; 
+plot(filtered_ecg, 'LineWidth', 2); 
+title('Filtered ECG Signal Lead I');
+xlabel('time in ms'); ylabel('voltage in mV');
+
+% Compute the power spectral density of the filtered ECG signal
+[pxx_filt, f_filt] = pwelch(filtered_ecg, [], [], [], fs, 'onesided');
+
+% Plot the power spectral density of the filtered ECG signal
+figure;
+plot(f_filt, 10*log10(pxx_filt));
+grid on;
+title('Power Spectral Density of Filtered ECG Signal');
+xlabel('Frequency (Hz)');
+ylabel('PSD (dB/Hz)');
 
 % Signal for saving
 signal = ecg;
